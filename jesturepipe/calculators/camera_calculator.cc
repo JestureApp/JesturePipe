8e8452d385a695c329aaaec2884522efe03de983
Carson Storm <carson.storm@outlook.com>
2022-12-09 13:48:39 -0700

    implement camera calculator

--- jesturepipe/calculators/camera_calculator.cc
+++ jesturepipe/calculators/camera_calculator.cc
@@ -3,18 +3,21 @@
 #include "mediapipe/framework/formats/image_frame.h"
 #include "mediapipe/framework/formats/image_frame_opencv.h"
 #include "mediapipe/framework/port/opencv_video_inc.h"
+#include "mediapipe/framework/tool/status_util.h"
+
+const char VideoStream[] = "VIDEO";
 
 // TODO: docs
 class CameraCalculator : public mediapipe::CalculatorBase {
    public:
     static absl::Status GetContract(mediapipe::CalculatorContract* cc) {
-        cc->Outputs().Tag("VIDEO").Set<mediapipe::ImageFrame>();
+        cc->Outputs().Tag(VideoStream).Set<mediapipe::ImageFrame>();
 
         return absl::OkStatus();
     }
 
-    absl::Status Open(mediapipe::CalculatorContext* cc) {
-        capture.open(0);
+    absl::Status Open(mediapipe::CalculatorContext* cc) override {
+        capture.open(2);
 
         if (!capture.isOpened()) {
             return absl::InternalError("Failed to open camera");
@@ -23,15 +26,13 @@ class CameraCalculator : public mediapipe::CalculatorBase {
         return absl::OkStatus();
     }
 
-    absl::Status Process(mediapipe::CalculatorContext* cc) {
-        cv::Mat camera_frame_raw;
+    absl::Status Process(mediapipe::CalculatorContext* cc) override {
         capture >> camera_frame_raw;
 
         if (camera_frame_raw.empty()) {
             return absl::OkStatus();
         }
 
-        cv::Mat camera_frame;
         cv::cvtColor(camera_frame_raw, camera_frame, cv::COLOR_BGR2RGB);
         cv::flip(camera_frame, camera_frame, /*flipcode=HORIZONTAL*/ 1);
 
@@ -47,18 +48,23 @@ class CameraCalculator : public mediapipe::CalculatorBase {
         size_t frame_timestamp =
             (double)cv::getTickCount() / (double)cv::getTickFrequency() * 1e6;
 
-        cc->Outputs().Tag("VIDEO").Add(output_frame.release(),
-                                       mediapipe::Timestamp(frame_timestamp));
+        cc->Outputs()
+            .Tag(VideoStream)
+            .Add(output_frame.release(), mediapipe::Timestamp(frame_timestamp));
 
         return absl::OkStatus();
     }
 
-    absl::Status Close(mediapipe::CalculatorContext* cc) {
+    absl::Status Close(mediapipe::CalculatorContext* cc) override {
+        capture.release();
+
         return absl::OkStatus();
     }
 
    private:
     cv::VideoCapture capture;
+    cv::Mat camera_frame_raw;
+    cv::Mat camera_frame;
 };
 
 REGISTER_CALCULATOR(CameraCalculator);
--- jesturepipe/examples/jesturepipe_demo.cc
+++ jesturepipe/examples/jesturepipe_demo.cc
@@ -1,12 +1,40 @@
 #include "absl/status/status.h"
 #include "jesturepipe/graphs/jesturepipe/jesturepipe.h"
 #include "mediapipe/framework/calculator_framework.h"
+#include "mediapipe/framework/formats/image_frame_opencv.h"
+#include "mediapipe/framework/port/opencv_highgui_inc.h"
+#include "mediapipe/framework/port/opencv_imgproc_inc.h"
+
+constexpr char kOutputStream[] = "annotated_frame";
+constexpr char kWindowName[] = "MediaPipe";
 
 absl::Status RunGraph() {
     mediapipe::CalculatorGraph graph;
     MP_RETURN_IF_ERROR(jesturepipe::jesturepipe_graph(&graph));
 
+    cv::namedWindow(kWindowName, 1);
+
+    ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller poller,
+                     graph.AddOutputStreamPoller(kOutputStream));
+
     MP_RETURN_IF_ERROR(graph.StartRun({}));
+
+    while (true) {
+        mediapipe::Packet packet;
+
+        if (!poller.Next(&packet)) break;
+        auto& output_frame = packet.Get<mediapipe::ImageFrame>();
+
+        cv::Mat output_frame_mat = mediapipe::formats::MatView(&output_frame);
+        cv::cvtColor(output_frame_mat, output_frame_mat, cv::COLOR_RGB2BGR);
+
+        cv::imshow(kWindowName, output_frame_mat);
+        const int pressed_key = cv::waitKey(5);
+
+        if (pressed_key >= 0 && pressed_key != 255) break;
+    }
+
+    MP_RETURN_IF_ERROR(graph.CloseAllPacketSources());
 
     return graph.WaitUntilDone();
 }
